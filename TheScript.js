17a7c350328bd9033612a727e7dadbb1fcf08cf0
CryptidWhale <156840605+CryptidWhale@users.noreply.github.com>
2024-01-17 11:39:12 +0800

    Add files via upload

---
+++ TheScript.js
@@ -0,0 +1,29 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+
+
+<script>
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
+
+<div id="main">
+  <button class="openbtn" onclick="openNav()">☰</button>  
+  <h2>Collapsed Sidebar</h2>
+  <p>Click on the hamburger menu/bar icon to open the sidebar, and push this content to the right.</p>
+</div>
+
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapse_sidebar
